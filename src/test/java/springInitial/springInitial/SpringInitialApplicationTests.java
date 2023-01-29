edac9341c61102431ce60b2f39be6a61fc53d88c
jeon3 <wldnjs3865@naver.com>
2023-01-29 20:49:09 +0900

    Initial commit

---
+++ src/test/java/springInitial/springInitial/SpringInitialApplicationTests.java
@@ -0,0 +1,13 @@
+package springInitial.springInitial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
