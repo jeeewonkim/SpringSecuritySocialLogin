f3abc007b328128f3f91495e9b1ff8d24c36fefc
jeon3 <wldnjs3865@naver.com>
2023-01-30 20:19:29 +0900

    현재 인증된 사용자의 정보 가져오기

---
+++ src/main/java/springInitial/springInitial/User/OAuthController.java
@@ -0,0 +1,22 @@
+package springInitial.springInitial.User;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/oauth")
+public class OAuthController {
+    @GetMapping("/loginInfo")
+    public String oauthLoginInfo(Authentication authentication){
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        return attributes.toString();
+    }
+}
